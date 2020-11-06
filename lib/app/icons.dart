660b74299e603d89d9adf04350db03af5bc7820a
ahtesham007 <zsyedahtesham@yahoo.com>
2020-11-06 19:45:25 +0530

    Face Mask Detection App Covid19

---
+++ lib/app/icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const IconData exit = Icons.exit_to_app;
+const IconData cameraFront = Icons.camera_front;
+const IconData cameraRear = Icons.camera_rear;
+const IconData code = Icons.info_outline;
+const IconData imageIcon = Icons.add_photo_alternate;
+const IconData cameraIcon = Icons.camera_enhance;
